0d9cee39d5769e2d658a084557e4ea8207e0541d
puargamer <ijkwok@ucsc.edu>
2023-05-01 18:50:51 -0700

    finished 1st slide, doing 2nd

--- script.js
+++ script.js
@@ -9,14 +9,19 @@ class Intro extends Phaser.Scene{
     }
 
     create() {
-        //load sprites
-        let text = this.add.text(400, 300, "Suspicious Games");
+
+        this.input.once('pointerdown', () => {
+            this.scene.start('loadingscreen');
+        });
+
+        //create sprites
+        let text = this.add.text(300, 280, "Suspicious Games", {font: "40px Georgia", color: "#eb4034"});
         let amogus1 = this.add.sprite(-200, 300, 'amogus');
         let amogus2 = this.add.sprite(1000, 300, 'amogus');
         let amogus3 = this.add.sprite(400, -200, 'amogus');
-        let amogus4 = this.add.sprite(300, 300, 'amogus');
+        let amogus4 = this.add.sprite(200, 300, 'amogus');
         
-        //settings
+        //among us settings
         amogus1.scale = amogus2.scale = amogus3.scale =  .25;
         amogus4.scale =.1;
 
@@ -26,11 +31,12 @@ class Intro extends Phaser.Scene{
 
         amogus4.setAlpha(0);
 
+        //audio
         let sound = this.sound.add('amongussound', { loop: false });
         sound.play();
 
         //animation chain
-        chain = this.tweens.chain({
+        let chain = this.tweens.chain({
             tweens: [
                 {
                     targets: amogus1,
@@ -66,9 +72,6 @@ class Intro extends Phaser.Scene{
             ]
         })
 
-        this.input.once('pointerdown', () => {
-            this.scene.start('loadingscreen');
-        });
     }
 }
 
@@ -78,16 +81,70 @@ class LoadingScreen extends Phaser.Scene{
     }
 
     preload() {
+        this.load.image('stunfisk', './assets/images/stunfisk.png');
+        this.load.image('fish', './assets/images/fish.png');
+        this.load.audio('water', './assets/audio/water.mp3');
+
 
     }
 
     create() {
-        this.add.text(20,20, "loading");
-    
+
         this.input.once('pointerdown', () => {
             this.scene.start('title');
         });
 
+        
+        //background
+        this.add.rectangle(400,300,800,600,0xa4c2f4);
+
+        //create sprites
+        
+        this.add.text(500,500, "loading", {font: "40px Georgia", color: "#000000"});
+        let fish1 = this.add.image(-200, 100, 'fish');
+        let fish2 = this.add.image(-200, 250, 'fish');
+        let fish3 = this.add.image(1000, 400, 'fish');
+        let stunfisk = this.add.image(690, 525, 'stunfisk');
+
+        //sprite settings
+        fish1.scale = fish2.scale = fish3.scale = .1;
+        fish3.flipX = true;
+        stunfisk.scale = .05;
+
+        //audio
+        let sound = this.sound.add('water', { loop: false });
+        sound.play();
+
+        //fish animation chain
+        let chain = this.tweens.chain({
+            tweens: [
+                {
+                    targets: fish1,
+                    x: 1000,
+                    duration: 800
+                },
+                {
+                    targets: fish2,
+                    x: 1000,
+                    duration: 800
+                },
+                {
+                    targets: fish3,
+                    x: -200,
+                    duration: 800
+                }
+            ]
+        })
+
+        //loading
+        this.add.tween({
+            targets: stunfisk,
+            angle: 360,
+            repeat: -1,
+            repeatDelay: 200
+        })
+
+        console.log("finished anim");
     }
 }
 
